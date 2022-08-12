0589b6e3c3952c4ccbe393f15dbf9ba83ca84f49
Kandyyy <kingkandy016@gmail.com>
2022-08-13 02:08:16 +0530

    Updated gitignore

--- create_table.py
+++ create_table.py
@@ -2,9 +2,11 @@ import sqlite3
 from eli import cur, con
 
 #DEFINING A DATABASE TABLE
-cur.execute(""" CREATE TABLE todos(
-            id integer,
-            todo text,
-            status text
-            )""")
+def CreateTable():
+    cur.execute(""" CREATE TABLE IF NOT EXISTS todos(
+                id integer,
+                todo text,
+                status text
+                )""")
 
+CreateTable()
